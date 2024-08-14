c89f7ba2ee621545d5a97168d34b23b872fc2b5e
Andrews Ferreira <51279454+andrewsferreira@users.noreply.github.com>
2024-08-14 10:56:03 -0300

    Update fw-teste.js

--- fw-teste.js
+++ fw-teste.js
@@ -4,7 +4,7 @@ const { Parser } = require('json2csv'); // Necessário para fazer a conversão e
 const path = require('path'); // Necessário para validar o 'path' de caminho de diretório
 
 // Diretório de arquivos
-const inputFilePath = path.join(__dirname, 'test-dataset.csv'); // Coloque o nome do CSV que irá carregar
+const inputFilePath = path.join(__dirname, 'test-dataset.csv'); // Coloque o nome do CSV que irá carregar, necessário estar o .csv na mesma pasta do script
 const blockedFilePath = path.join(__dirname, 'blocked-ips.csv');
 const flaggedFilePath = path.join(__dirname, 'flagged-ips.csv');
 const allowedFilePath = path.join(__dirname, 'allowed-ips.csv');
